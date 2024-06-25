0703b308c4f567beb9213167e2cfc184d8771b7c
Eric Skaftason <skaftasoneric@gmail.com>
2024-06-24 22:20:43 -0400

    Commit 2

---
+++ controllers/tetris_controller.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    const body = req.body;
+    console.log(body);
+
+    res.json({ status: 'Message received' });
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+//INITIALIZE
+
+const tetris_controller = require('./routes/tetris_controller.js');
+app.use('/tetris', tetris_controller);
