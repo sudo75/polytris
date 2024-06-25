cf3632931a20c3d30964207592d93c0061a574f5
Eric Skaftason <skaftasoneric@gmail.com>
2024-06-24 23:18:57 -0400

    commit 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -9,3 +10,14 @@ app.use(bodyParser.json());
 
 const tetris_controller = require('./controllers/tetris_controller.js');
 app.use('/tetris', tetris_controller);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
