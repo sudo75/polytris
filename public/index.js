882f04158d7f68e7ae727e7b7b6b38d01abc8869
smash <skaftasoneric@gmail.com>
2024-07-17 10:51:20 +0900

    2024-07-17_commit.10

--- public/index.js
+++ public/index.js
@@ -40,7 +40,7 @@ class Game {
 
     start(id) {
         fetch(`tetris/start`, {
-            method: "POST",
+            method: "PUT",
             headers: {
                 'Content-Type': 'application/json'
             },
@@ -69,7 +69,7 @@ class Game {
     requestNewFrame() {
         this.sendReq(
             'tetris/reqFrame',
-            'POST',
+            'PUT',
             JSON.stringify({ id: this.id }),
             (data) => {
                 this.displayFrame(data.frame, data.frame_preClear, data.status);
